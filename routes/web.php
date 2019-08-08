f5056a65921ea3d29cb4c8b87fdae1afa3210176
RANGGA_PR <rangga_pr@pop-os.localdomain>
2019-08-08 15:02:09 +0700

    model and controller

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,9 @@
 
 
 Route::get('/','DataController@index')->name('data.all');
+Route::get('/home_user', 'User@index');
+Route::get('/login', 'User@login');
+Route::post('/loginPost', 'User@loginPost');
+Route::get('/register', 'User@register');
+Route::post('/registerPost', 'User@registerPost');
+Route::get('/logout', 'User@logout');
