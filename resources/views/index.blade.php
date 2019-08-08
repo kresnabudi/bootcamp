fbac6d7094c1873dbf524a1b493ea908840d16f0
winataralali <winata.aham@ralali.com>
2019-08-08 16:12:01 +0700

    fix conflict

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,29 +1,4 @@
-
-ini adalah coba
-{{-- <!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
-
-</body>
-</html> --}}
 @extends('head')
 @extends('body')
 @section('footer')
-<<<<<<< HEAD
 @endsection
-=======
-coba lg deh
-t
-t
-g
-y
-y
-a
-@endsection
->>>>>>> 9a67e56bb9d48ef2f2f2c65a63463448f108ab28
