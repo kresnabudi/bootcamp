8d03a6c471679fd361b29e579d9f2234710d28b9
Kresna <kb.hartanto@gmail.com>
2019-08-08 15:17:13 +0700

    add conflig

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,25 @@
+
+ini adalah coba
+{{-- <!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html> --}}
 @extends('head')
 @extends('body')
 @section('footer')
 coba lg deh
-@endsection
 t
 t
 g
 y
 y
 a
+@endsection
