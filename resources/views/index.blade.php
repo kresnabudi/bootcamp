dbfcb485d4ee354299d86900c38d4c588abce897
Kresna <kb.hartanto@gmail.com>
2019-08-09 10:26:42 +0700

    add confict

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,26 +1,12 @@
+<h1>Hello World!</h1>
+<p>Hello, selamat pagi, selamat datang, hehehe.</p>
 @extends('head')
+<h1>Hello World!</h1>
+<p>Hello, selamat pagi, selamat datang, hehehe.</p>
 @extends('body')
 <h1>Hello World!</h1>
 <p>Hello, selamat pagi, selamat datang, hehehe.</p>
 @section('footer')
-coba lg deh
-t
-ini adalah coba
-{{-- <!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
-
-</body>
-</html> --}}
-t
-g
-y
-y
-a
+<h1>Hello World!</h1>
+<p>Hello, selamat pagi, selamat datang, hehehe.</p>
 @endsection
