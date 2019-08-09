654726a3f5773621e45e0565f326eb31947654e2
Kresna <kb.hartanto@gmail.com>
2019-08-09 10:59:18 +0700

    add confict

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,2 +1,12 @@
 <h1>Hello World!</h1>
 <p>Hello, selamat pagi, selamat datang, hehehe.</p>
+@extends('head')
+<h1>Hello World!</h1>
+<p>Hello, selamat pagi, selamat datang, hehehe.</p>
+@extends('body')
+<h1>Hello World!</h1>
+<p>Hello, selamat pagi, selamat datang, hehehe.</p>
+@section('footer')
+<h1>Hello World!</h1>
+<p>Hello, selamat pagi, selamat datang, hehehe.</p>
+@endsection
