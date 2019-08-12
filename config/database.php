95fd21012a0c5e1f5f4fb754dfc1d353bf5d9c32
winataralali <winata.aham@ralali.com>
2019-08-12 11:04:39 +0700

    create constant

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => constant('DB_HOST', '127.0.0.1'),
+            'port' => constant('DB_PORT', '3306'),
+            'database' => constant('DB_DATABASE', 'forge'),
+            'username' => constant('DB_USERNAME', 'forge'),
+            'password' => constant('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
