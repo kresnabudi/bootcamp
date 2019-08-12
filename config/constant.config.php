58b52fa1a1d03a540e01d9b43554688cebe830a0
RANGGA_PR <rangga_pr@pop-os.localdomain>
2019-08-12 11:09:35 +0700

    change db host

--- config/constant.config.php
+++ config/constant.config.php
@@ -1,6 +1,6 @@
 <?php
 
-if(!defined('DB_HOST')) define('DB_HOST','ralali');
+if(!defined('DB_HOST')) define('DB_HOST','localhost');
 if(!defined('DB_DATABASE')) define('DB_DATABASE','ralali');
 if(!defined('DB_USERNAME')) define('DB_USERNAME','root');
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD','b2bmarketplace');
