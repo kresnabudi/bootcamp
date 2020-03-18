0e739326644ff7b68c507f4f786cb7ad94d66114
Kresna <kb.hartanto@gmail.com>
2020-03-18 17:25:47 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,20 +1,4 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-@extends('head')
-
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <title>Ralali Bootcamp</title>
-
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-  <!-- Styles -->
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
+@extends('headear')
 
 <body>
   <header class="site-header">
